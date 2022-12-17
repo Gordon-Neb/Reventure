93e4d2ca9af461a47c4937c0b3468017a91b32a7
Gordon-Neb <gordon.neb20@gmail.com>
2022-12-17 23:14:36 +0100

    Add variable jump height

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,6 +15,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
+		
 	if is_on_floor(): 
 		jumps = jumpCount
 			
@@ -25,8 +26,9 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and jumps > 0:
 		velocity.y = JUMP_VELOCITY
 		jumps = jumps -1
-		print(jumps)
-		
+	if Input.is_action_just_released("Jump") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY/2
+	
 		
 	if Input.is_action_pressed("Right"):
 			velocity.x = MAX_SPEED
