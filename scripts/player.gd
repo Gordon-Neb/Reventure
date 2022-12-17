f594c70f4b88005ea5e5b2ec24f5a83752976798
Gordon-Neb <gordon.neb20@gmail.com>
2022-12-17 22:54:24 +0100

    Updating Test Level

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const MAX_SPEED = 100
 const ACCELERATION = 1
 const JUMP_VELOCITY = -400.0
 var jumpCount = 2
+var jumps = 0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -13,17 +14,26 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if is_on_floor(): 
+		jumps = jumpCount
+			
+		
+		
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("Jump", true) and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and jumps > 0:
 		velocity.y = JUMP_VELOCITY
+		jumps = jumps -1
+		print(jumps)
+		
 		
 	if Input.is_action_pressed("Right"):
 			velocity.x = MAX_SPEED
-			print(velocity.x)
+			#print(velocity.x)
 	elif Input.is_action_pressed("Left"):
 			velocity.x = -MAX_SPEED
-			print(velocity.x)
+			#print(velocity.x)
 	else:
 		velocity.x = move_toward(velocity.x, 0, MAX_SPEED)
 		
